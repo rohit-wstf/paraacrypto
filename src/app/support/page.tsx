e7a0d02544019d7be1c9c48a72f6568dd50efcd1
rohit-wstf <rohit.roshan@thewasserstoff.com>
2025-01-27 17:47:18 +0530

    updated hero section

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -9,7 +9,7 @@ const ContactUs = () => {
       <div className="w-full">
         <div className="container-wrapper mx-auto px-3 py-9 flex flex-col gap-12 ">
           <Address />
-          <section className="  flex flex-col lg:flex-row items-center gap-6 md:gap-10 md:p-[60px]">
+          <section className="  flex flex-col lg:flex-row gap-6 md:gap-10 md:p-[60px]">
             <div className="flex flex-col flex-1 gap-4">
               <h2 className="font-bold text-[28px] sm:text-[45px] text-primaryText">
                 Write To Us.{" "}
